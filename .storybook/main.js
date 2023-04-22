8c80dab24ca4cc077990041cc2241134653c35b4
ki504178 <ki.504178@gmail.com>
2023-04-22 10:14:36 +0900

    chore: package アップデート

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,19 @@
-const path = require('path')
-
+const path = require('path');
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
-  webpackFinal: async (config) => {
-    config.resolve.alias = { ...(config.resolve.alias || []), '@': path.resolve(__dirname, '../src') }
-
-    return config
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-mdx-gfm'],
+  webpackFinal: async config => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || []),
+      '@': path.resolve(__dirname, '../src')
+    };
+    return config;
   },
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5',
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
   },
-}
+  docs: {
+    autodocs: true
+  }
+};
