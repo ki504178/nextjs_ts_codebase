4cfa9484c1b50834ef9b5f4c0066143f50b9c2da
ki504178 <ki.504178@gmail.com>
2024-09-09 09:50:24 +0900

    [all] feat: パッケージ最新化・Biome移行

--- src/components/Sample.stories.tsx
+++ src/components/Sample.stories.tsx
@@ -1,9 +1,12 @@
-import { ComponentStoryObj } from '@storybook/react'
-import { Sample } from './Sample'
+import { ComponentStoryObj } from "@storybook/react";
+import { Sample } from "./Sample";
 
-type Story = ComponentStoryObj<typeof Sample>
+type Story = ComponentStoryObj<typeof Sample>;
 
-const config = { component: Sample, args: { title: 'Next.jsサンプル', description: '設定楽ちん' } }
-export default config
+const config = {
+	component: Sample,
+	args: { title: "Next.jsサンプル", description: "設定楽ちん" },
+};
+export default config;
 
-export const Default: Story = {}
+export const Default: Story = {};
