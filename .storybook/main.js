41e28176e262ae9c5e57d87b91e8d9c2665c6ec0
ki504178 <47514232+ki504178@users.noreply.github.com>
2024-09-09 10:01:21 +0900

    [all] feat: パッケージ最新化・Biome移行 (#20)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,30 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-mdx-gfm'],
-  webpackFinal: async config => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || []),
-      '@': path.resolve(__dirname, '../src')
-    };
-    return config;
-  },
-  framework: {
-    name: '@storybook/nextjs',
-    options: {}
-  },
-  docs: {
-    autodocs: true
-  }
+	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/addon-interactions",
+		"@storybook/addon-mdx-gfm",
+		"@chromatic-com/storybook",
+	],
+
+	webpackFinal: async (config) => {
+		config.resolve.alias = {
+			...(config.resolve.alias || []),
+			"@": path.resolve(__dirname, "../src"),
+		};
+		return config;
+	},
+
+	framework: {
+		name: "@storybook/nextjs",
+		options: {},
+	},
+
+	docs: {},
+
+	typescript: {
+		reactDocgen: "react-docgen-typescript",
+	},
 };
